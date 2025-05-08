24e736a88429dd49d70fb822fe22a7193a0420cc
Alexander Winblad <alai20@student.bth.se>
2025-05-08 12:28:54 +0200

    Fixed so tests is not mocking validator

--- backend/test/written/test_get_user_by_email.py
+++ backend/test/written/test_get_user_by_email.py
@@ -12,9 +12,7 @@ class TestGetUserByEmail:
         mockedDAO.find.return_value = [user]
         uc = UserController(dao=mockedDAO)
 
-        with patch('src.controllers.usercontroller.re.fullmatch') as mockfullmatch:
-            mockfullmatch.return_value = True
-            assert uc.get_user_by_email(email=email) == user
+        assert uc.get_user_by_email(email=email) == user
     
     def test_2(self):
         email = 'user@example.com'
@@ -32,57 +30,51 @@ class TestGetUserByEmail:
         mockedDAO = MagicMock()
         mockedDAO.find.return_value = [user1]
         uc = UserController(dao=mockedDAO)
-        with patch('src.controllers.usercontroller.re.fullmatch') as mockfullmatch:
-            mockfullmatch.return_value = True
-            assert uc.get_user_by_email(email=email) == user1
+
+        assert uc.get_user_by_email(email=email) == user1
 
     def test_4(self):
         email = 'userexample.com'
         mockedDAO = MagicMock()
         mockedDAO.find.return_value = []
         uc = UserController(dao=mockedDAO)
-        with patch('src.controllers.usercontroller.re.fullmatch') as mockfullmatch:
-            mockfullmatch.return_value = False
-            with pytest.raises(ValueError):
-                uc.get_user_by_email(email=email)
+
+        with pytest.raises(ValueError):
+            uc.get_user_by_email(email=email)
         
     def test_5(self):
         email = 'user@'
         mockedDAO = MagicMock()
         mockedDAO.find.return_value = []
         uc = UserController(dao=mockedDAO)
-        with patch('src.controllers.usercontroller.re.fullmatch') as mockfullmatch:
-            mockfullmatch.return_value = False
-            with pytest.raises(ValueError):
-                uc.get_user_by_email(email=email)
+
+        with pytest.raises(ValueError):
+            uc.get_user_by_email(email=email)
     
     def test_6(self):
         email = '@example.com'
         mockedDAO = MagicMock()
         mockedDAO.find.return_value = []
         uc = UserController(dao=mockedDAO)
-        with patch('src.controllers.usercontroller.re.fullmatch') as mockfullmatch:
-            mockfullmatch.return_value = False
-            with pytest.raises(ValueError):
-                uc.get_user_by_email(email=email)
+
+        with pytest.raises(ValueError):
+            uc.get_user_by_email(email=email)
 
     def test_7(self):
         email = ''
         mockedDAO = MagicMock()
         mockedDAO.find.return_value = []
         uc = UserController(dao=mockedDAO)
-        with patch('src.controllers.usercontroller.re.fullmatch') as mockfullmatch:
-            mockfullmatch.return_value = False
-            with pytest.raises(ValueError):
-                uc.get_user_by_email(email=email)
+
+        with pytest.raises(ValueError):
+            uc.get_user_by_email(email=email)
 
     def test_8(self):
         email = 'user@example.com'
         mockedDAO = MagicMock()
         mockedDAO.find.side_effect = Exception('Database error')
         uc = UserController(dao=mockedDAO)
-        with patch('src.controllers.usercontroller.re.fullmatch') as mockfullmatch:
-            mockfullmatch.return_value = True
-            with pytest.raises(Exception):
-                uc.get_user_by_email(email=email)
+
+        with pytest.raises(Exception):
+            uc.get_user_by_email(email=email)
                 
