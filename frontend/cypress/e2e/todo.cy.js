406b55968d179b354cbbb39bba0bd888dd365456
Alexander Winblad <alwi12399@gmail.com>
2025-06-30 23:14:00 +0200

    Fixed test number 2

--- frontend/cypress/e2e/todo.cy.js
+++ frontend/cypress/e2e/todo.cy.js
@@ -3,69 +3,63 @@ describe('Todo tests', () => {
     let uid // user id
     let name // name of the user (firstName + ' ' + lastName)
     let email // email of the user
+    let testNumber // what number the test is to find the correct task
+    let tasks // tasks that are in the database
   
     before(function () {
-      // create a fabricated user from a fixture
-      cy.fixture('user.json')
-        .then((user) => {
-          cy.request({
-            method: 'POST',
-            url: 'http://localhost:5000/users/create',
-            form: true,
-            body: user
-          }).then((response) => {
-            uid = response.body._id.$oid
-            name = user.firstName + ' ' + user.lastName
-            email = user.email
-          })
-        })
-    })
+      testNumber = 0
+      cy.request('POST', 'http://localhost:5000/populate').then((response) => {
+        uid = response.body.users[0];
 
-    before(function () {
-      cy.visit('http://localhost:3000')
-      cy.contains('div', 'Email Address')
-        .find('input[type=text]')
-        .type(email)
+        // Now make the second request using uid
+        cy.request('GET', `http://localhost:5000/users/${uid}`).then((response) => {
+          email = response.body.email;
+          name = response.body.firstName + " " + response.body.lastName;
 
-      cy.get('form')
-        .submit()
-    
-      cy.contains('div', "Title")
-        .find('input[type=text]')
-        .type("Testing Task")
+          // You can log or use email and name here
+          cy.log(`User: ${name}, Email: ${email}`);
+        });
+        cy.request('GET', `http://localhost:5000/tasks/ofuser/${uid}`).then((response) => {
+          tasks = response.body
+          console.log(response.body)
+        });
+      });
 
-      cy.contains('div', 'YouTube URL')
-        .find('input[type=text]')
-        .type('https://youtu.be/pRh-21VLreI?si=6LfDa0RUCes2xz4U')
-
-      cy.get('form')
-        .submit()
-
-    })
+    });
+      
   
     beforeEach(function () {
-      // enter the main main page
+      // // enter the main main page
+      // cy.visit('http://localhost:3000')
+      // cy.contains('div', 'Email Address')
+      //   .find('input[type=text]')
+      //   .type(email)
+  
+      // // submit the form on this page
+      // cy.get('form')
+      //   .submit()
+
+      // // Doing last item when testing so it actually test the newest added item
+      // cy.get('div.title-overlay')
+      //   .filter(':contains("Testing Task")')
+      //   .last()
+      //   .closest('.container-element')
+      //   .click();
+
+      // cy.get('.popup')
+      //   .should('be.visible')
       cy.visit('http://localhost:3000')
       cy.contains('div', 'Email Address')
         .find('input[type=text]')
         .type(email)
-  
-      // submit the form on this page
+
       cy.get('form')
         .submit()
-
-      // Doing last item when testing so it actually test the newest added item
-      cy.get('div.title-overlay')
-        .filter(':contains("Testing Task")')
-        .last()
-        .closest('.container-element')
-        .click();
-
-      cy.get('.popup')
-        .should('be.visible')
     })
 
     it('if description empty do not add', () => {
+      cy.contains('div.title-overlay', `${tasks[0].title}`).click({ force: true });
+        
       cy.get('.popup form.inline-form input[type="text"]')
         .clear()
         .should('have.value', '');
@@ -75,60 +69,64 @@ describe('Todo tests', () => {
         .should('be.disabled');
     })
 
+   it('adding item to todo list', () => {
+      const task = tasks[0];
+      console.log(task);
 
-    it('adding item to todo list', () => {
+      const newTodo = { taskid: task._id.$oid, description: 'HELLO', done: 'false' };
+      console.log(newTodo);
 
-      cy.get('.popup')
-        .find('form.inline-form input[type="text"]')
-        .type('Test Adding Task');
+      cy.request({
+        method: 'POST',
+        url: `http://localhost:5000/todos/create`,
+        form: true,
+        body: newTodo
+      }).then((postResponse) => {
+        expect(postResponse.status).to.eq(200);
+        const createdTodo = postResponse.body;
 
-      cy.get('.popup')
-        .find('form.inline-form')
-        .submit();
+        expect(createdTodo.description).to.eq(newTodo.description);
+        expect(String(createdTodo.done)).to.eq(newTodo.done);
+      });
+    });
 
-      cy.get('li.todo-item')
-        .should('contain.text', 'Test Adding Task')
-    })
+    // it('setting todo item to done', () => {
 
-    
+    //   cy.contains('li.todo-item', 'Test Adding Task')
+    //     .find('span.checker')
+    //     .should('have.class', 'unchecked')
 
-    it('setting todo item to done', () => {
+    //   cy.contains('li.todo-item', 'Test Adding Task')
+    //     .find('span.checker')
+    //     .click()
 
-      cy.contains('li.todo-item', 'Test Adding Task')
-        .find('span.checker')
-        .should('have.class', 'unchecked')
+    //   cy.contains('li.todo-item', 'Test Adding Task')
+    //     .find('span.checker')
+    //     .should('have.class', 'checked')
+    // })
 
-      cy.contains('li.todo-item', 'Test Adding Task')
-        .find('span.checker')
-        .click()
+    // it('setting todo item to active', () => {
 
-      cy.contains('li.todo-item', 'Test Adding Task')
-        .find('span.checker')
-        .should('have.class', 'checked')
-    })
+    //   cy.contains('li.todo-item', 'Test Adding Task')
+    //     .find('span.checker')
+    //     .should('have.class', 'checked')
 
-    it('setting todo item to active', () => {
+    //   cy.contains('li.todo-item', 'Test Adding Task')
+    //     .find('span.checker')
+    //     .click()
 
-      cy.contains('li.todo-item', 'Test Adding Task')
-        .find('span.checker')
-        .should('have.class', 'checked')
+    //   cy.contains('li.todo-item', 'Test Adding Task')
+    //     .find('span.checker')
+    //     .should('have.class', 'unchecked')
+    // })
 
-      cy.contains('li.todo-item', 'Test Adding Task')
-        .find('span.checker')
-        .click()
+    // it('delete todo item', () => {
+    //   cy.contains('li.todo-item', 'Test Adding Task')
+    //     .find('span.remover')
+    //     .click()
 
-      cy.contains('li.todo-item', 'Test Adding Task')
-        .find('span.checker')
-        .should('have.class', 'unchecked')
-    })
-
-    it('delete todo item', () => {
-      cy.contains('li.todo-item', 'Test Adding Task')
-        .find('span.remover')
-        .click()
-
-      cy.get('ul.todo-list')
-        .should('not.contain', 'Test Adding Task')
-    })
+    //   cy.get('ul.todo-list')
+    //     .should('not.contain', 'Test Adding Task')
+    // })
 
 })
